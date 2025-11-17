54dcb92011e71e58ee757ff41d61135153eced31
Mahmoud khaled <164625524+Mahmoudkhaled17@users.noreply.github.com>
2025-11-17 19:15:56 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -13,3 +13,85 @@ function showe(vara){
 
     }
 }
+
+function showtap(vex){
+    switch (vex){
+            case "tap":
+            console.log(document.getElementsByClassName("hash")[0].style.display)
+            if(String(document.getElementsByClassName("hash")[0].style.display)=="" ||String(document.getElementsByClassName("hash")[0].style.display)=="none" ){
+            document.getElementsByClassName("hash")[0].style.display='flex'
+            document.getElementsByClassName("about-me")[0].style.display='none'
+            document.getElementsByClassName("Education")[0].style.display='none'
+            document.getElementsByClassName("Experances")[0].style.display='none'
+            document.getElementsByClassName("projects")[0].style.display='none'
+            document.getElementsByClassName("contact-me")[0].style.display='none'
+            break;}
+            else {
+            document.getElementsByClassName("hash")[0].style.display='none'
+            document.getElementsByClassName("about-me")[0].style.display='block'
+
+            document.getElementsByClassName("Education")[0].style.display='block'
+            document.getElementsByClassName("Experances")[0].style.display='block'
+            document.getElementsByClassName("projects")[0].style.display='flex'
+            document.getElementsByClassName("contact-me")[0].style.display='block'
+            break;
+            }
+
+    
+}
+
+}
+
+
+function showtape(ver){
+    switch (ver){
+
+        case "about":
+        document.getElementsByClassName("about-me")[0].style.display='block'
+        document.getElementsByClassName("Education")[0].style.display='none' 
+        document.getElementsByClassName("hash")[0].style.display='none'
+        document.getElementsByClassName("contact-me")[0].style.display='none'
+        document.getElementsByClassName("projects")[0].style.display='none'
+        document.getElementsByClassName("Experances")[0].style.display='none'
+        break;
+
+        case "ed":
+        document.getElementsByClassName("about-me")[0].style.display='none'
+        document.getElementsByClassName("Education")[0].style.display='block' 
+        document.getElementsByClassName("hash")[0].style.display='none'
+        document.getElementsByClassName("contact-me")[0].style.display='none'
+        document.getElementsByClassName("projects")[0].style.display='none'
+        document.getElementsByClassName("Experances")[0].style.display='none'
+        break;
+
+        case "ex":
+         document.getElementsByClassName("about-me")[0].style.display='none'
+        document.getElementsByClassName("Education")[0].style.display='none' 
+        document.getElementsByClassName("hash")[0].style.display='none' 
+        document.getElementsByClassName("Experances")[0].style.display='block' 
+        document.getElementsByClassName("projects")[0].style.display='none'
+        document.getElementsByClassName("contact-me")[0].style.display='none'
+      
+          break;
+
+        case "pr":
+            document.getElementsByClassName("about-me")[0].style.display='none'
+            document.getElementsByClassName("Education")[0].style.display='none'
+            document.getElementsByClassName("Experances")[0].style.display='none'
+            document.getElementsByClassName("projects")[0].style.display='flex'
+            document.getElementsByClassName("contact-me")[0].style.display='none'
+            document.getElementsByClassName("hash")[0].style.display='none'
+            break;
+           
+
+            case "con":
+            document.getElementsByClassName("about-me")[0].style.display='none'
+            document.getElementsByClassName("Education")[0].style.display='none'
+            document.getElementsByClassName("Experances")[0].style.display='none'
+            document.getElementsByClassName("projects")[0].style.display='none'
+            document.getElementsByClassName("contact-me")[0].style.display='block'
+            document.getElementsByClassName("hash")[0].style.display='none'
+            break;
+             }
+    
+}
